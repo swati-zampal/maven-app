e79242178d512e7209830ffeed956b88c29a5ccf
swati-zampal <131242062+swati-zampal@users.noreply.github.com>
2026-01-02 11:33:19 +0530

    Update App.java

--- src/main/com/fortune/app/App.java
+++ src/main/com/fortune/app/App.java
@@ -5,7 +5,7 @@ package com.fortune.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!, Happy New Year";
 
     public App() {}
 
